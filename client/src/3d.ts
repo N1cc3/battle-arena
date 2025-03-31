4b3fff5f05e384765ec7169c9818c9ef6878ad07
N1cc3 <niclaslindgren91@hotmail.com>
2025-04-01 01:53:11 +0300

    Add thor model with animations

--- client/src/3d.ts
+++ client/src/3d.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { getGLTF } from './assets'
+import { models } from './assets'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
@@ -9,8 +9,14 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
+window.addEventListener('resize', () => {
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
 const controls = new OrbitControls(camera, renderer.domElement)
-camera.position.set(0, 20, 50)
+camera.position.set(0, 5, 10)
 controls.autoRotate = true
 controls.update()
 
@@ -19,13 +25,20 @@ scene.add(light)
 
 const clock = new THREE.Clock()
 
-getGLTF('forest').then((gltf) => {
-	scene.add(gltf.scene)
-})
-
 const animate = () => {
 	const delta = clock.getDelta()
 	controls.update(delta)
+	models.thor.mixer.update(delta)
 	renderer.render(scene, camera)
 }
-renderer.setAnimationLoop(animate)
+
+Promise.all([
+	models.forest.load().then(() => {
+		scene.add(models.forest.gltf.scene)
+		models.forest.gltf.scene.position.set(0, -5, 0)
+	}),
+	models.thor.load().then(() => {
+		scene.add(models.thor.gltf.scene)
+		models.thor.play('Run_Fwd_C')
+	}),
+]).then(() => renderer.setAnimationLoop(animate))
--- client/src/assets.tsx
+++ client/src/assets.tsx
@@ -1,16 +1,57 @@
+import * as THREE from 'three'
 import { GLTF, GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
-const gltf = {
-	forest: '/forest/forest.gltf',
+const gltfLoader = new GLTFLoader()
+
+class GLTFModel<M extends string, A extends string> {
+	gltf!: GLTF
+	mixer!: THREE.AnimationMixer
+	constructor(private _name: M, private _animations: A[]) {}
+	get name() {
+		return this._name
+	}
+	get animations() {
+		return this._animations
+	}
+	async load() {
+		this.gltf = await gltfLoader.loadAsync(`assets/${this.name}/scene.gltf`)
+		this.mixer = new THREE.AnimationMixer(this.gltf.scene)
+	}
+	play(anim: A) {
+		const clip = THREE.AnimationClip.findByName(this.gltf.animations, anim)
+		if (!clip) throw new Error(`Animation ${anim} not found on model ${this.name}`)
+		return this.mixer.clipAction(clip).play()
+	}
 }
 
-const gltfLoader = new GLTFLoader()
-export const getGLTF = (name: keyof typeof gltf) =>
-	new Promise<GLTF>((resolve, reject) => {
-		gltfLoader.load(
-			gltf[name],
-			(gltf) => resolve(gltf),
-			() => {},
-			reject
-		)
+export const models = {
+	forest: new GLTFModel('forest', []),
+	thor: new GLTFModel('thor', [
+		'Like_Personality',
+		'Idle_C',
+		'Like_Idle',
+		'Emote_10398002020',
+		'Emote_10395002030',
+		'Emote_10390012010',
+		'103961_ThunderRelease_Loop',
+		'Run_Fwd_C',
+		'Walk_Fwd_C',
+		'Sleep',
+	]),
+} as const
+
+export const getGLTF = (model: keyof typeof models) => gltfLoader.loadAsync(`assets/${model}/scene.gltf`)
+
+export const dumpObject = (obj: OBJ, lines: string[] = [], isLast = true, prefix = '') => {
+	const localPrefix = isLast ? '└─' : '├─'
+	lines.push(`${prefix}${prefix ? localPrefix : ''}${obj.name || '*no-name*'} [${obj.type}]`)
+	const newPrefix = prefix + (isLast ? '  ' : '│ ')
+	const lastNdx = obj.children.length - 1
+	obj.children.forEach((child, ndx) => {
+		const isLast = ndx === lastNdx
+		dumpObject(child, lines, isLast, newPrefix)
 	})
+	return lines
+}
+
+type OBJ = { name?: string; type?: string; children: OBJ[] }
